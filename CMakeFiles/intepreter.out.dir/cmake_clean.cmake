ece7423349b63b0f2c809b97132c022fcae269bf
mihamor <mishamoroz5@gmail.com>
2019-03-16 00:04:50 +0200

    removed cmakefiles

--- CMakeFiles/intepreter.out.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/intepreter.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/mylya.out.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mylya.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
